482c25c6bcb139be3419749c7f6cef848b1b2c06
Jose Latapiatt <jose.latapiatt@gmail.com>
2020-08-23 20:11:47 -0400

    Added zoom websdk

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// import { ZoomMtg } from 'https://source.zoom.us/zoom-meeting-1.7.10.min.js';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  
+// ZoomMtg.preLoadWasm();
+// ZoomMtg.prepareJssdk();
